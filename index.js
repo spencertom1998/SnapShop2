a729319312befc79230fa65edc3ae1ffde1734e7
Spencer <Spencer@stores-mbp.wifi.bcit.ca>
2017-11-01 15:07:45 -0700

    port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var client = amazon.createClient({
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.listen(port, function(err){
     if(err){
