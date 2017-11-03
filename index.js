a43c83f1943d54eda7f44f371355cd9af518f724
Spencer <Spencer@stores-MacBook-Pro.local>
2017-11-02 18:02:12 -0700

    test

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ var client = amazon.createClient({
 
 const express = require("express");
 const app = express();
-const port = 3000;
-//const port = process.env.PORT||3000;
+const port = process.env.PORT||3000;
+<?php header('Access-Control-Allow-Origin: *'); ?>
 
 
 
