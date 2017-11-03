5ff98db2b45838d5e70c7345e47901f240337ab8
Spencer <Spencer@stores-MacBook-Pro.local>
2017-11-02 17:59:08 -0700

    test

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ var client = amazon.createClient({
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT||3000;
+const port = 3000;
+//const port = process.env.PORT||3000;
 
 
 
